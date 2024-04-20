72501caf6537486a40ff14b8e47fc35e6cd83a09
Steven <srmcnamara7@gmail.com>
2024-04-19 22:27:47 -0400

    just puttering

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -3,7 +3,7 @@ import Service from "./ui/Service";
 export default function Page() {
     const serviceItems = [
         {
-            name: "acupuncture",
+            name: "Acupuncture",
             description:
                 "An acupunture session with one of our top practitioners",
             cost: 35,
@@ -25,8 +25,10 @@ export default function Page() {
     return (
         <>
             <div className="flex flex-col md:flex-row md:overflow-hidden">
-                <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-                    <p>Services:</p>
+                <div className="flex-grow p-6 md:overflow-y-auto ">
+                    <div className="justify-center flex font-sans text-2xl font-medium">
+                        Services
+                    </div>
                     {serviceItems.map((item) => {
                         console.log("hi hello");
                         return (
--- app/services/ui/Service.jsx
+++ app/services/ui/Service.jsx
@@ -1,9 +1,15 @@
 export default function Service({ title, description, cost }) {
     return (
-        <div className="rounded-xl content-between bg-gray-200 p-4 m-2 shadow-sm">
-            <div className="text-lg font-medium">{title}</div>
-            <div className="text-md text-green-800">{description}</div>
-            <div className="text-sm text-green-500">${cost}.00</div>
+        <div className="rounded-xl  flex-row content-between bg-gray-100 p-4 m-2 shadow-sm">
+            <div className="text-lg text-pink-500 text-center font-medium justify-center flex">
+                {title}
+            </div>
+            <div className="text-md text-center text-purple-800 justify-center flex">
+                {description}
+            </div>
+            <div className="text-sm text-center text-indigo-500 justify-center flex">
+                ${cost}.00
+            </div>
         </div>
     );
 }
