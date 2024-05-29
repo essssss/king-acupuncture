621a5a636e6fc8aaf14649fb20a3914f4def3cfd
essssss <73257027+essssss@users.noreply.github.com>
2024-05-28 22:33:49 -0400

    more tweaks

--- app/about/ui/bio.js
+++ app/about/ui/bio.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Bio({ name, bio, headshot }) {
     return (
         <div className="rounded-xl flex-row md:flex justify-between bg-gradient-to-br from-indigo-100 via-purple-100 to-pink-100 p-4 m-2  shadow-sm flex-grow">
-            <div className="p-3 bg-white rounded-l-md  ">
+            <div className="p-3 bg-white rounded-t-md md:rounded-l-md  ">
                 <div className="text-lg text-pink-500  text-center font-medium justify-center flex">
                     {name}
                 </div>
